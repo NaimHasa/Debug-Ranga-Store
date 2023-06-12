860a6ccbd04e7b03094448dc793240a1e2afa72f
Naim Hasan <naimarafat7@gmail.com>
2023-06-12 14:37:04 -0700

    totalpriceFixed

--- js/app.js
+++ js/app.js
@@ -45,9 +45,10 @@ let count = 0;
 
 const addToCart = (id, price) => {
    count = count + 1;
-   updatePrice('price', value);
+   updatePrice('price', price);
 
    updateTaxAndCharge();
+   updateTotal();
    document.getElementById('total-Products').innerText = count;
 };
 
@@ -75,9 +76,9 @@ const getInputValue = (id) => {
 // main price update function
 const updatePrice = (id, value) => {
    const convertedOldPrice = getInputValue(id);
-   const convertPrice = parseInt(value);
+   const convertPrice = parseFloat(value);
    const total = convertedOldPrice + convertPrice;
-   document.getElementById(id).innerText = Math.round(total);
+   document.getElementById(id).innerText = total.toFixed(2);
 };
 
 // set innerText function
@@ -109,7 +110,9 @@ const updateTotal = () => {
       getInputValue('delivery-charge') +
       getInputValue('total-tax');
    document.getElementById('total').innerText = grandTotal;
+
 };
+
 
 // search by category
 document.getElementById("search-btn").addEventListener("click", function () {
